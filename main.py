845517ce2502b8430d805d0d357a0f77b71dd668
Mike <ms53150@gmail.com>
2022-05-03 20:12:50 -0400

    Add try-except to check if user enters letters and not numbers

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_dict = data.set_index("letter")["code"].to_dict()
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter word: ").upper()
-alpha_list = [phonetic_dict[letter] for letter in word]
 
-print(alpha_list)
+# Create a list of the phonetic code words from a word that the user inputs.
+
+def generate_phonetic():
+    word = input("Enter word: ").upper()
+    try:
+        alpha_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(alpha_list)
+
+
+generate_phonetic()
